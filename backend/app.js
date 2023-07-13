dc60da4c7e60519ca2b778f3e5c0644c53653f58
KwameSei <nathanielosei92@gmail.com>
2023-07-13 05:09:14 +0000

    Basic backend server configuration done

---
+++ backend/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+
+dotenv.config();
+
+const app = express();
+
+export default app;
