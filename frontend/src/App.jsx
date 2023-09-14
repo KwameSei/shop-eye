7bcce9404bdcc9134ab63c19c51c3dcbf02e5546
KwameSei <nathanielosei92@gmail.com>
2023-09-14 09:52:20 +0000

    Landing page route changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -76,7 +76,7 @@ function App() {
         <Routes>
           <Route path='/login' element={<Auth onSignIn={handleSignIn} />} />
           {/* <Route path='/register' element={<Auth onSignIn={handleSignIn} />} /> */}
-          <Route path='/' element={<Login />} />
+          <Route path='/' element={<Auth />} />
           <Route path='/profile/:id' element={<Profile />} />
           {/* <SecuredRoute path="/dashboard" element={<Dashboard />} isSignedIn={isSignedIn} /> */}
           {/* <Route path="/dashboard" element={ <ProtectedRoute isSignedIn={isSignedIn}> <Dashboard /> </ProtectedRoute> } /> */}
